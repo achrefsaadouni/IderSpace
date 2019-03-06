425b9f0dca2ef3687c7023d269b493ec333fddc3
Unknown <mohamed.benmalekcherif@esprit.tn>
2019-03-06 15:06:08 +0100

    01

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,6 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
 const userRoutes = require("./routes/user");
-const eventRoutes = require("./routes/events");
-const demandRoutes = require("./routes/demands");
 
 const app = express();
 
@@ -25,26 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/images", express.static(path.join("backend/images")));
-
-app.use("api/chat", (req, res, next) => {
-  s;
-  req.js({
-    msg: "ok"
-  });
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
 
 app.use("/api/user", userRoutes);
 
