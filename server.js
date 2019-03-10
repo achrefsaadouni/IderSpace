bf50e18b3748b3c52e7c337d00f147c71bab28aa
Med ben Malek <mohamed.benmalekcherif@esprit.tn>
2019-03-10 14:10:18 +0100

    testing chat bot API
    Using Dialogflow and apiai plugin

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require("http");
 const debug = require("debug")("node:server");
 const app = require("./backend/app");
 
+var socket = require("socket.io");
+var ServiceChatbot = require("./backend/services/ServiceChatbot");
+
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -52,4 +55,32 @@ const server = http.createServer(app);
 
 server.on("error", onError);
 server.on("listening", onListening);
+
+//chatbot API
+
 var serverpost = server.listen(port);
+
+io = socket(serverpost);
+io.on("connection", socket => {
+  //______socket.on event SEND_MESSAGE
+  socket.on("SEND_MESSAGE", function(data) {
+    //console.log('data sent form user' + JSON.stringify(data));
+
+    //________call the function to return the response from the bot ___//
+    ServiceChatbot.askQuestion(data.msg)
+      .then(response => {
+        //console.log('the respons is' + JSON.stringify(response))
+        //_____switch intent Name we will execute the correct function __________//
+        console.log(response.result.fulfillment.speech);
+
+        io.emit("RECEIVE_MESSAGE", {
+          msg: response.result.fulfillment.speech
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        var message = "Error from the server !" + error;
+        io.emit("RECEIVE_MESSAGE", message);
+      });
+  });
+});
