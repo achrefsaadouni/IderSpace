7e12e41f6d084ec87bae40c325d959017fef3a84
Achref <achref.saadouni@esprit.tn>
2019-04-04 13:05:10 +0100

    chatbot v2

--- backend/controllers/chatbot.js
+++ backend/controllers/chatbot.js
@@ -1,4 +1,6 @@
 const chatbot = require('../services/chatbot')
 exports.chat = (req, res, next) => {
- res.send(chatbot.createTextResponse("iderspace is a platform"));
+ res.send(chatbot.createTextResponse("The goals of IderSpace are clear and simple, provide comfortable features to Our Members\n" +
+     "within its quarters with a modern touch. The assistant will be a chat bot ready to respond to\n" +
+     "user’s commands and an advanced Recommendation system."));
 };
