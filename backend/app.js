1a1726245094bdc0e3f7442cef48fb5fc40fb3c1
Med ben Malek <mohamed.benmalekcherif@esprit.tn>
2019-04-04 15:30:12 +0100

    qv0.1

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-var db = require('./Models/Db');
+var db = require("./Models/Db");
 var cors = require("cors");
 
 const userRoutes = require("./routes/user");
 const ActivitiesRoutes = require("./routes/activity");
+const QuestRoutes = require("./routes/question");
 
 const app = express();
 
 app.use(cors());
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use("/images", express.static(path.join("backend/images")));
 
 app.use("/api/user", userRoutes);
+app.use("/api/question", QuestRoutes);
 app.use("/api/activity", ActivitiesRoutes);
 
 module.exports = app;
