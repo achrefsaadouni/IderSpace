2e8bca14930baf4700f72ce877ee0915a7ef64a3
Achref <achref.saadouni@esprit.tn>
2019-04-05 09:33:38 +0100

    training chatbot

--- backend/Models/BotQuestion.js
+++ backend/Models/BotQuestion.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 const BotQuestionSchema = mongoose.Schema({
     createdAt: { type: Date, required: true },
-    content: { type: String, required: true },
+    content: { type: String, required: true ,unique : true},
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
