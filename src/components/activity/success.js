5ccb65bec752b07de2ee0f4f082d120e3edb048a
Ismail <ismail.yahia@esprit.tn>
2019-05-03 12:05:20 +0100

    fixing imports activity

--- src/components/activity/success.js
+++ src/components/activity/success.js
@@ -8,13 +8,6 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 
 
 class success extends Component {
-
-
-    constructor() {
-        super()
-
-
-    }
     componentDidMount(){
         const {values} = this.props;
         this.props.CreateActivity(values);
